f0517e75657737fc6f16897ec32ab3b8e99baee1
zohaibzyebi <zyebi553@gmail.com>
2023-02-10 23:03:26 +0500

    testing:fixes in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[100vh] ssm:h-[930px] sxl:h-[100vh] flex flex-col items-center justify-end  sxl:justify-center  px-4 ssm:px-6  lg:px-8 bg-[url('/images/hero-bg-sm.svg')] sxl:bg-[url('/images/hero-bg.svg')] bg-no-repeat bg-hero-sm sxl:bg-center bg-cover bg-black  relative">
+    <div className="w-full h-[96vh] ssm:h-[930px] sxl:h-[100vh] flex flex-col items-center justify-end  sxl:justify-center  px-4 ssm:px-6  lg:px-8 bg-[url('/images/hero-bg-sm.svg')] sxl:bg-[url('/images/hero-bg.svg')] bg-no-repeat bg-hero-sm sxl:bg-center bg-cover bg-black  relative">
       <div className="flex justify-start items-center absolute top-5 left-4 ssm:left-6 lg:left-8">
         {/* => Logo  */}
         <div className="w-[120px] h-[32px] relative flex items-center justify-center object-contain">
           <Image src="/images/logo.svg" alt="" fill />
         </div>
       </div>
-      <div className="w-full flex flex-col items-start justify-start gap-5 md:gap-8 ssm:px-2 sm:px-4 md:px-6 lg:px-8 pb-12 sxl:pb-0 sxl:pt-16 ">
+      <div className="w-full flex flex-col items-start justify-start gap-5 md:gap-8 ssm:px-2 sm:px-4 md:px-6 lg:px-8 pb-16 sxl:pb-0 sxl:pt-16 ">
         <h1 className="text-[65px] ssm:text-[80px] md:text-[100px] text-white font-bold leading-[66px] ssm:leading-[81px] md:leading-[90px] lg:leading-[82px]">
           Posiziona <br className="hidden md:inline" />
           il tuo brand <br className="hidden md:inline" />
