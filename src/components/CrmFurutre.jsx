50dfb2c67211292036917c296c1087ff2399b41c
zohaibzyebi <zyebi553@gmail.com>
2023-02-11 14:15:44 +0500

    done: crm future section

--- src/components/CrmFurutre.jsx
+++ src/components/CrmFurutre.jsx
@@ -1,46 +1,37 @@
-import React, { useState, useEffect } from "react";
-import ReactPlayer from "react-player/lazy";
+import React from "react";
 
 const CrmFurutre = () => {
-  // State to check the window. The ReactPlayer will only be rendered when there is windo available. It is done to the hydration error given by the ReacPlayer due to Next.js pre-rendering feature.
-  const [hasWindow, setHasWindow] = useState(false);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setHasWindow(true);
-    }
-  }, []);
   return (
-    <div className="w-full flex flex-col items-center justify-center py-16 px-16 bg-black">
-      <div className="w-full flex flex-col items-start justify-start gap-8">
-        <div className="w-full flex flex-col items-start justify-start gap-8">
-          <h2 className="text-[65px] laeding-[66px] text-white font-bold text-left">
+    <div className="w-full flex flex-col items-center justify-center py-6 ssm:py-16 px-4 ssm:px-6 lg:px-8 mxl:px-16 xxl:px-40 bg-black">
+      <div className="w-full grid lg:grid-cols-1 mxl:grid-cols-[1.3fr_0.7fr] gap-12 ssm:gap-16 mxl:gap-4">
+        <div className="w-full flex flex-col items-start justify-start gap-5 ssm:gap-8 xxl:gap-12">
+          <h2 className="text-[50px] ssm:text-[65px] xxl:text-[100px] leading-[50px] ssm:leading-[63px] xxl:leading-[100px] text-white font-medium ssm:font-bold text-left">
             Il futuro del CRM
           </h2>
-          <h3 className="w-[50%] text-left text-mercury text-[50px] leading-[51px]">
+          <h3 className="lg:w-[65%] mxl:w-[80%] xxl:w-[75%] text-left text-mercury text-[32px] ssm:text-[50px] xxl:text-[60px] leading-[32px] ssm:leading-[51px] xxl:leading-[61px]">
             Una porta d’accesso ad un’esperienza di shopping mai vista prima.
           </h3>
-          <div className="w-full flex flex-col items-start justify-start gap-4">
+          <div className="lg:w-full flex flex-col items-start justify-start gap-4 xxl:gap-8">
             <h4 className="text-left text-white text-[30px]  leading-[31px] font-bold">
               Riconosci e premia i tuoi clienti fedeli
             </h4>
-            <p className="w-[50%] text-left text-softPeach text-[20px] leading-[21px] ">
+            <p className="lg:w-[65%] mxl:w-[95%] xxl:w-[75%] text-left text-softPeach text-[16px] ssm:text-[20px] leading-[19px] ssm:leading-[21px] ">
               Incentivate le comunità e mantenetele affamate del vostro marchio.
               Offrite ai possessori di token l’accesso anticipato ai drop o
               l’accesso esclusivo a merch in edizione limitata, esperienze e
               altro ancora quando collegano un portafoglio al vostro negozio.
             </p>
-            <p className="w-[50%] text-left text-softPeach text-[20px] leading-[21px] ">
+            <p className="lg:w-[65%] mxl:w-[95%] xxl:w-[75%] text-left text-softPeach text-[16px] ssm:text-[20px] leading-[19px] ssm:leading-[21px]">
               Aumenta il valore medio del carrello dei tuoi clienti offrendo
               offerte irrununciabili ai possessori degli NFT al momento del
               checkout.
             </p>
           </div>
-          <div className="w-full flex flex-col items-start justify-start gap-4">
+          <div className="w-full flex flex-col items-start justify-start gap-4 xxl:gap-8">
             <h4 className="text-left text-white text-[30px]  leading-[31px] font-bold">
               Unisci le community e collaborare con altri brand
             </h4>
-            <p className="w-[50%] text-left text-softPeach text-[20px] leading-[21px] ">
+            <p className="lg:w-[65%] mxl:w-[95%] text-left text-softPeach text-[16px] ssm:text-[20px] leading-[19px] ssm:leading-[21px] ">
               Rilasciate prodotti dedicati alle comunità che si riuniscono
               dietro il vostro marchio.
               <br />
@@ -49,18 +40,16 @@ const CrmFurutre = () => {
             </p>
           </div>
         </div>
-        <div className="w-full flex items-center justify-center ">
-          {hasWindow && (
-            <div className="w-[425px] h-[425px] rounded-lg overflow-hidden relative">
-              <ReactPlayer
-                className="react-player"
-                controls={false}
-                playing
-                url="/videos/crm-future.mp4"
-                loop
-              />
-            </div>
-          )}
+        <div className="w-full flex items-center mxl:items-start vxl:items-center justify-center mxl:justify-end xxl:pt-16 vxl:pt-0">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="w-[320px] ssm:w-[425px] h-[320px] ssm:h-[425px]  xxl:w-[576px] xxl:h-[576px] rounded-md overflow-hidden"
+          >
+            <source src="/videos/crm-future.mp4" type="video/mp4" />
+          </video>
         </div>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="w-full min-h-[800px] ssm:min-h-0 h-[100vh] ssm:h-[930px] sxl:h-[100vh] flex flex-col items-center justify-end  sxl:justify-center  px-4 ssm:px-6  lg:px-8 bg-[url('/images/hero-bg-sm.svg')] sxl:bg-[url('/images/hero-bg.svg')] bg-no-repeat bg-hero-sm sxl:bg-center bg-cover bg-black  relative">
-      <div className="w-full flex justify-between lg:justify-start items-center absolute top-4 ssm:top-5 left-0 px-4 ssm:px-6 lg:px-8">
+    <div className="w-full min-h-[800px] ssm:min-h-0 h-[100vh] ssm:h-[930px] lg:h-[100vh] flex flex-col items-center justify-end  lg:justify-center  px-4 ssm:px-6 lg:px-8 mxl:px-16 bg-[url('/images/hero-bg-sm.svg')] sxl:bg-[url('/images/hero-bg.svg')] bg-no-repeat bg-hero-sm sxl:bg-center bg-cover bg-black  relative">
+      <div className="w-full flex justify-between lg:justify-start items-center absolute top-3 xs:top-4 ssm:top-5 left-0 px-4 ssm:px-6 lg:px-8 mxl:px-16">
         {/* => Logo  */}
         <div className="w-[120px] h-[32px] relative flex items-center justify-center object-contain">
           <Image src="/images/logo.svg" alt="" fill />
@@ -14,7 +14,7 @@ const Hero = () => {
           <Image src="/icons/menu.svg" alt="" width={20} height={10} />
         </div>
       </div>
-      <div className="w-full flex flex-col items-start justify-start gap-5 md:gap-8 ssm:px-2 sm:px-4 md:px-6 lg:px-8 pb-16 sxl:pb-0 sxl:pt-20 ">
+      <div className="w-full flex flex-col items-start justify-start gap-5 md:gap-8  pb-16 sxl:pb-0 lg:pt-28 ">
         <h1 className="text-[55px] ssm:text-[80px] md:text-[100px] text-white font-bold leading-[56px] ssm:leading-[81px] md:leading-[90px] lg:leading-[82px]">
           Posiziona <br className="hidden md:inline" />
           il tuo brand <br className="hidden md:inline" />
